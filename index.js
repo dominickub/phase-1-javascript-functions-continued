d32fe93b486c09e990eb6d0e20c49e484b9381ea
dominick <dominickugas@gmail.com>
2021-10-13 09:21:20 -0400

    commit tree 2b8a6ec35f582cbb5903f7aa0c4b7e053db971c1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function saturdayFun(a = "roller-skate") {
+    return `This Saturday, I want to ${a}!`
+}
+
+function mondayWork(a = 'go to the office') {
+    return `This Monday, I will ${a}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function (string = "special") {
+        return `You are ${flair}${string}${flair}!`
+    }
+
+}
